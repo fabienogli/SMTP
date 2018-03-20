d1ec268153b44b87faf5b6306e1df4866414e992
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2018-03-20 14:58:51 +0100

    add view to write email

--- src/Interface/main/WritingPage.java
+++ src/Interface/main/WritingPage.java
@@ -1,4 +1,30 @@
 package Interface.main;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class WritingPage {
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private Button attachButton;
+
+    @FXML
+    private Button SaveButton;
+
+    @FXML
+    private TextField senderTextField;
+
+    @FXML
+    private TextField recipientTextField;
+
+    @FXML
+    private TextField subjectTextField;
+
+    @FXML
+    private TextArea corpsTextArea;
 }
