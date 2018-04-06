1f2ef93dc5a9a2d49cd3e685f874baa05d0260e4
Mark Kpamy <markkpamy@gmail.com>
2018-04-06 12:15:31 +0200

    r

--- src/startAllServers.java
+++ src/startAllServers.java
@@ -12,6 +12,7 @@ public class startAllServers {
         ServerPop3 serverPop3 = new ServerPop3();
         //registerDns(serverSMTP.getDns());
         Thread t = new Thread(serverSMTP);
+
         Thread t2 = new Thread(serverPop3);
         t.start();
         t2.start();
